a552e652fdba66b3c6c0b53a0fe680334138d340
Oleksandr Voronkov <ovoronkov@geeksforless.com>
2023-11-05 18:48:36 +0200

    3rdlabdone

---
+++ OOPLR3/OOPLR3/Form1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualBasic;
+
+namespace OOPLR3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Telephone.Count.ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(new MobilePhone().Info());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(new HomeTelephone().Info());
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(new Telephone().Info());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var weight = double.Parse(Interaction.InputBox("Введіть вагу мобільного телефону:", "Введення"));
+                var width = double.Parse(Interaction.InputBox("Введіть ширину мобільного телефону:", "Введення"));
+                var height = double.Parse(Interaction.InputBox("Введіть висоту мобільного телефону:", "Введення"));
+                var name = Interaction.InputBox("Введіть назву мобільного телефону:", "Введення");
+                var mp = new MobilePhone(width, height, weight, name);
+                MessageBox.Show(mp.Info());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var weight = double.Parse(Interaction.InputBox("Введіть вагу стаціонарного телефону:", "Введення"));
+                var width = double.Parse(Interaction.InputBox("Введіть ширину стаціонарного телефону:", "Введення"));
+                var height = double.Parse(Interaction.InputBox("Введіть висоту стаціонарного телефону:", "Введення"));
+                var name = Interaction.InputBox("Введіть назву стаціонарного телефону:", "Введення");
+                var sp = new HomeTelephone(width, height, weight, name);
+                MessageBox.Show(sp.Info());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка");
+            }
+        }
+    }
+}
---
+++ OOPLR3/OOPLR3/Telephone.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLR3
+{
+    internal class Telephone
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public static int Count { get; set; }
+        public string Name { get; set; }
+        public int Nomer { get; set; }
+        public Telephone(double width = 0, double height = 0, double weight = 0, string name = "Telephone") 
+        {
+            Count++;
+            Nomer = Count;
+            Width = width;
+            Height = height;
+            Weight = weight;
+            Name = name;
+        }
+
+        public string Info()
+        {
+            return $"Телефон {Name}, із шириною {Width}, висотою {Height}, вагою {Weight}, та номером {Nomer}";
+        }
+    }
+
+    internal sealed class HomeTelephone : Telephone
+    {
+        public HomeTelephone(double width = 0, double height = 0, double weight = 0, string name = "Стаціонарний телефон") 
+            : base(width, height, weight, name)
+        {
+
+        }
+    }
+
+    internal sealed class MobilePhone : Telephone
+    {
+        public MobilePhone(double width = 0, double height = 0, double weight = 0, string name = "Мобільний телефон")
+            : base(width, height, weight, name)
+        {
+
+        }
+    }
+}
